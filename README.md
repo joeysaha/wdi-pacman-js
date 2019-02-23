016a86886cf4879655b7429d9a92f78cfe390665
Joey Saha <joeysaha@hotmail.com>
2019-02-22 23:42:26 -0500

    done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Pac-Man JS
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/wdi/67/assignments/2050/latest)
+
+working pacman game
--- pacman.js
+++ pacman.js
@@ -1,10 +1,11 @@
 // Setup initial game stats
 let score = 0;
 let lives = 2;
+let powerPellet = 4;
 
 
 // Define your ghosts here
-var inky = {
+let inky = {
   menu_option: '1',
   name: 'Inky',
   color: 'Red',
@@ -12,7 +13,7 @@ var inky = {
   edible: false
 };
 
-var blinky = {
+let blinky = {
   menu_option: '2',
   name: 'Blinky',
   color: 'Cyan',
@@ -20,7 +21,7 @@ var blinky = {
   edible: false
 };
 
-var pinky = {
+let pinky = {
   menu_option: '3',
   name: 'Pinky',
   color: 'Pink',
@@ -28,7 +29,7 @@ var pinky = {
   edible: false
 };
 
-var clyde = {
+let clyde = {
   menu_option: '4',
   name: 'Clyde',
   color: 'Orange',
@@ -37,11 +38,12 @@ var clyde = {
 }
 
 // replace this comment with your four ghosts setup as objects
-
+let ghosts = [inky, blinky, pinky, clyde]
 
 // Draw the screen functionality
 function drawScreen() {
   clearScreen();
+  gameOver();
   setTimeout(() => {
     displayStats();
     displayMenu();
@@ -54,20 +56,27 @@ function clearScreen() {
 }
 
 function displayStats() {
-  console.log(`Score: ${score}     Lives: ${lives}`);
+  console.log(`Score: ${score}     Lives: ${lives} \n\nPower-Pellets: ${powerPellet}`);
 }
 
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
+  if (powerPellet > 0 ) {
+    console.log('(p) Eat Power-Pellet');
+  }
+  for (var i = 1; i <= 4; i++) {
+    // outputs "(#) Eat ____"
+    console.log(`(${i}) Eat ${ghosts[i-1].name} (${ghosts[i-1].edible ? 'edible' : 'inedible'})`);
+  };
   console.log('(q) Quit');
-}
+};
+
 
 function displayPrompt() {
   // process.stdout.write is similar to console.log except it doesn't add a new line after the text
   process.stdout.write('\nWaka Waka :v '); // :v is the Pac-Man emoji.
 }
-
 
 // Menu Options
 function eatDot() {
@@ -75,6 +84,36 @@ function eatDot() {
   score += 10;
 }
 
+function eatGhost(ghost) {
+  // ghost.edible ? console.log('Chomperz') : lives--;
+  if (ghost.edible === true) {
+    console.log(`\nChomperz! The ${ghost.color} ${ghost.name} ghost was eaten.`);
+    score += 200;
+    ghost.edible = false;
+  } else {
+    console.log(`\nOh no! The ${ghost.color} ${ghost.name} ghost took one of your lives!`);
+    lives--;
+  }
+}
+
+function eatPowerPellet() {
+  if (powerPellet <= 0) {
+    console.log('\nThere are no more Power-Pellets left!');
+  } else {
+    score += 50;
+    powerPellet--;
+    for (let i = 0; i < ghosts.length; i++) {
+      ghosts[i].edible = true;
+    };
+    console.log('\nConsumed power pellet. All ghosts are edible now!');
+  }
+};
+
+function gameOver() {
+  if (lives <= 0) {
+    process.exit();
+  }
+};
 
 // Process Player's Input
 function processInput(key) {
@@ -85,6 +124,21 @@ function processInput(key) {
       break;
     case 'd':
       eatDot();
+      break;
+    case '1':
+      eatGhost(ghosts[0]);
+      break;
+    case '2':
+      eatGhost(ghosts[1]);
+      break;
+    case '3':
+      eatGhost(ghosts[2]);
+      break;
+    case '4':
+      eatGhost(ghosts[3]);
+      break;
+    case 'p':
+      eatPowerPellet();
       break;
     default:
       console.log('\nInvalid Command!');
@@ -109,7 +163,7 @@ drawScreen();
 stdin.on('data', (key) => {
   process.stdout.write(key);
   processInput(key);
-  setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
+  setTimeout(drawScreen, 1200); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
 });
 
 // Player Quits
